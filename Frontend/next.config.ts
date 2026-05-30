4c60494ff3526582e466a59bea72f2a91c808941
khaicybers <khaicybers@gmail.com>
2026-05-30 17:56:20 +0700

    fix img

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     transpilePackages: ["antd", "@ant-design/icons", "@ant-design/pro-components", "rc-util", "rc-pagination", "rc-picker"],
     images: {
+        unoptimized: true,
         formats: ["image/avif", "image/webp"],
         qualities: [65, 70, 72, 75],
         minimumCacheTTL: 60 * 60 * 24 * 30,
